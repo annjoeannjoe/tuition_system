3e16382384a1c07e9642d242a70744a124273908
annjoeannjoe <annjoe2001@gmail.com>
2023-11-02 22:14:07 +0800

    urls changes

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+   path('', views.login, name="login"),
    path('login/', views.login, name="login"),
    path('logintesting/', views.login, name="logintesting"),
    path('register/', views.register, name="register"),
