2ef382a14c00d13b310d9845b24aef5c064cf132
annjoeannjoe <annjoe2001@gmail.com>
2024-01-08 22:45:01 +0800

    delete

--- users/views.py
+++ users/views.py
@@ -1230,7 +1230,7 @@ def delete_admin(request, pk):
     user = get_object_or_404(User, id=pk, role='ADMIN')
 
     # Delete the associated user object
-    user.soft_delete()
+    user.delete()
 
     return redirect('admin_list_view')
 
